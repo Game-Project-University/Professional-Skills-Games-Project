3b09ff49fa3bfb96bcdfff4702f1ff1604c1913a
Blood-Raven <crabmanification@hotmail.co.uk>
2015-02-05 19:00:10 +0000

    Added Program/Front-End/Game Functions

--- Game_Project/Game_Project.cpp
+++ Game_Project/Game_Project.cpp
@@ -38,62 +38,126 @@
 //--INCLUDES--//
 #include <TL-Engine.h>	// TL-Engine include file and namespace
 #include <sstream>
-
-//--USING NAMESPACES--//
 using namespace tle;
 
-//--COSTANT GLOBAL VARIABLES--//
+////////////////////////
+//--GLOBAL VARIABLES--//
+
+I3DEngine* myEngine;
+IFont* MyFont;
+ISprite* sprite;
+ICamera* myCamera;
+
+////////////////
+//--CONSTANTS-//
+
 /* KeyCodes*/
 const EKeyCode EXIT = Key_Escape;
+
 /*Font Variables*/
 // Positions of the FPS Text
 const float FontFpsX = 40.0f;
 const float FontFpsY = 0.0f;
 
-//--MAIN FUNCTION--//
-void main()
+//////////////////////////////
+//--PROGRAM SETUP/SHUTDOWN--//
+bool ProgramSetup()
 {
 	//--TL:ENGINE INITIALISATION--//
 	// Create a 3D engine (using TLX engine here) and open a window for it
-	I3DEngine* myEngine = New3DEngine( kTLX );
+	myEngine = New3DEngine(kTLX);
 	// Create window can alter the size 
+	if (!myEngine)
+	{
+		return false;
+	}
 	myEngine->StartWindowed(1280, 900);
-
+	
 	//--MEDIA FILE DIRECTORIES--//
 	// Add default folder for meshes and other media
-	myEngine->AddMediaFolder( "C:\\ProgramData\\TL-Engine\\Media" );
+	myEngine->AddMediaFolder("C:\\ProgramData\\TL-Engine\\Media");
 	myEngine->AddMediaFolder(".\\Media");
 
-	//****--SCENE SETUP--****//
+	return true;
+}
+
+// Shutdown for the whole program
+void ProgramShutdown()
+{
+	//--ENGINE DELETION--//
+	myEngine->Delete();
+}
+
+////////////////////////////////
+//--FRONT END SETUP/SHUTDOWN--//
+void FrontEndSetup()
+{
+
+}
+
+void FrontEndUpdate()
+{
+
+}
+
+void FrontEndShutdown()
+{
+
+}
+
+///////////////////////////
+//--GAME SETUP/SHUTDOWN--//
+void GameSetup()
+{
+	//--LOAD MESH/SPRITES--//
+	sprite = myEngine->CreateSprite("lol.png");
+	sprite->SetZ(0);
+	
+	//--CREATE SCENE--//
+
+
 	//--CAMERA CREATION--//
-	ICamera* myCamera = myEngine->CreateCamera(kManual); //Creation of kFPS camera
+	myCamera = myEngine->CreateCamera(kManual); //Creation of kFPS camera
 
 	//--LOAD FONT--///
-	stringstream interfaceText;
-	IFont* MyFont = myEngine->LoadFont("Comic Sans MS", 36.0f);
+	MyFont = myEngine->LoadFont("Comic Sans MS", 36.0f);
+}
 
-	ISprite* sprite = myEngine->CreateSprite("lol.png");
-
-	sprite->SetZ(0);
-
-	//--LOAD MESH--//
-
+void GameUpdate()
+{
 	//--FRAMETIME VARIABLE--//
 	float frameTime;
+	//--TIMER INTIALISING--//
+	frameTime = myEngine->Timer();
+
+	//--STATS INTERFACE--//
+	stringstream interfaceText;
+	interfaceText << "FPS: " << 1 / frameTime;
+	MyFont->Draw(interfaceText.str(), FontFpsX, FontFpsY);
+	interfaceText.str("");
+}
+
+void GameShutdown()
+{
+	myEngine->RemoveFont(MyFont);
+	myEngine->RemoveCamera(myCamera);
+	myEngine->RemoveSprite(sprite);
+}
+
+//--MAIN FUNCTION--//
+void main()
+{
+	ProgramSetup();
+	
+	GameSetup();
 
 	//--MAIN GAME LOOP--//
 	while (myEngine->IsRunning())
 	{
-		//--TIMER INTIALISING--//
-		frameTime = myEngine->Timer();
-
-		//--STATS INTERFACE--//
-		interfaceText << "FPS: " << 1/frameTime;
-		MyFont->Draw(interfaceText.str(), FontFpsX, FontFpsY);
-		interfaceText.str("");
-
 		//--DRAWN SCENE--//
 		myEngine->DrawScene();
+
+		GameUpdate();
 
 		//--Exit Statement--//
 		if (myEngine->KeyHit(EXIT))
@@ -101,6 +165,7 @@ void main()
 			myEngine->Stop();
 		}
 	}
-	//--ENGINE DELETION--//
-	myEngine->Delete();
+
+	GameShutdown();
+	ProgramShutdown();
 }
