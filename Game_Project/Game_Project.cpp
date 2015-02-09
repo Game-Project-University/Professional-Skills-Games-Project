3c10da6fef2227b72341d3e05fd46ef42bc11a4d
hawkzey <james-gregory22@hotmail.com>
2015-02-09 17:00:54 +0000

    Added Sound

--- Game_Project/Game_Project.cpp
+++ Game_Project/Game_Project.cpp
@@ -12,22 +12,22 @@ ICamera* myCamera;
 
 // A buffer holds sound data, but the creation of a buffer on its own doesn't play a sound. It is the
 // equivalent of a mesh in the TL-Engine
-//ALuint buffer;
+ALuint buffer;
 
 // A source is an actual sound in the world. A sound must be associated with a buffer to indicate
-// which sound data to play. Sources are equivalent to models in the TL-Engine
-//ALuint source;
+//which sound data to play. Sources are equivalent to models in the TL-Engine
+ALuint source;
 
 // Each source has several properties, see the code for examples. Here we store position and velocity of
 // the sound source above (x, y & z)
-//ALfloat sourcePos[3] = { 0.0, 0.0, 0.0 };
-//ALfloat sourceVel[3] = { 0.0, 0.0, 0.0 };
+ALfloat sourcePos[3] = { 0.0, 0.0, 0.0 };
+ALfloat sourceVel[3] = { 0.0, 0.0, 0.0 };
 
 // There is always assumed to be a listener in an OpenAL application. We don't need a specific listener
 // variable. However, listeners also have properties (examples in code). Here we store the position and
 // velocity of the listener
-//ALfloat listenerPos[3] = { 0.0, 0.0, 0.0 };
-//ALfloat listenerVel[3] = { 0.0, 0.0, 0.0 };
+ALfloat listenerPos[3] = { 0.0, 0.0, 0.0 };
+ALfloat listenerVel[3] = { 0.0, 0.0, 0.0 };
 
 // The listener may be at an angle (which may affect the perception of sound). Here we store the 
 // orientation of the listener. The first three values are the facing direction (x, y, z) of the
@@ -35,8 +35,8 @@ ICamera* myCamera;
 // of the listener, called "up". These vectors can be extracted from a world or view matrix
 // NOTE: OpenAL (like OpenGL) uses a right-handed system for 3D coordinates. To convert from the
 // left-handed system  we have used, we must negate all Z values (facing direction has -ve Z below)
-//ALfloat listenerOri[6] = { 0.0, 0.0, -1.0,
-//0.0, 1.0, 0.0 };
+ALfloat listenerOri[6] = { 0.0, 0.0, -1.0,
+0.0, 1.0, 0.0 };
 
 ///////////////
 //--OBJECTS--//
@@ -78,7 +78,7 @@ bool ProgramSetup()
 	// Initialise ALUT and hence OpenAL. If not using ALUT, we can initialise using core OpenAL functions.
 	// However then we would need to consider what sound devices are available, with what features, and
 	// select from those. ALUT just selects the default output device
-	//alutInit(0, 0);
+	alutInit(0, 0);
 
 
 	//****************
@@ -86,35 +86,35 @@ bool ProgramSetup()
 
 	// Create a sound buffer by loading a sound file. Using ALUT again to simplify this process. ALUT makes
 	// its best attempt to convert different file formats into an OpenAL data format
-	//buffer = alutCreateBufferFromFile("tada.wav");
+	buffer = alutCreateBufferFromFile(".\\media\\tada.wav");
 
 
 	//****************
 	// Sources
 
 	// Create one sound source
-	//alGenSources(1, &source);
+	alGenSources(1, &source);
 
 	// Set the properties of the source. The full list of available properties can be found in the documentation
 	// The last characters of each function name indicate the type of the second parameter (int, float, float vector etc.)
-	//alSourcei(source, AL_BUFFER, buffer); // Attach a buffer to the source (identify which sound to play)
-	//alSourcef(source, AL_PITCH, 1.0f);   // Pitch multiplier, doubling the pitch shifts the sound up 1 octave, halving
+	alSourcei(source, AL_BUFFER, buffer); // Attach a buffer to the source (identify which sound to play)
+	alSourcef(source, AL_PITCH, 1.0f);   // Pitch multiplier, doubling the pitch shifts the sound up 1 octave, halving
 	// the pitch shifts it down 1 octave. Will also shorten/lengthen the sound
-	//alSourcef(source, AL_GAIN, 1.0f);   // Effectively the volume of the sound - 0.0 = silent, 1.0 = as recorded. May
+	alSourcef(source, AL_GAIN, 1.0f);   // Effectively the volume of the sound - 0.0 = silent, 1.0 = as recorded. May
 	// be able to increase volume over 1, but depends on sound
-	//alSourcefv(source, AL_POSITION, sourcePos); // Position of sound relative to listener affects how it is reproduced through speakers
-	//alSourcefv(source, AL_VELOCITY, sourceVel); // Velocity of sound relative to listener can cause Doppler effect
-	//alSourcei(source, AL_LOOPING, AL_FALSE);  // Whether to loop the sound or just stop when it finishes
+	alSourcefv(source, AL_POSITION, sourcePos); // Position of sound relative to listener affects how it is reproduced through speakers
+	alSourcefv(source, AL_VELOCITY, sourceVel); // Velocity of sound relative to listener can cause Doppler effect
+	alSourcei(source, AL_LOOPING, AL_FALSE);  // Whether to loop the sound or just stop when it finishes
 
 
 	//****************
 	// Listener
 
 	// Set the properties of the listener. These are all the available listener properties
-	//alListenerfv(AL_POSITION, listenerPos); // Position, velocity and orientation of listener affect sound...
-	//alListenerfv(AL_VELOCITY, listenerVel); // ...reproduction as noted above
-	//alListenerfv(AL_ORIENTATION, listenerOri);
-	//alListenerf(AL_GAIN, 1.0f);        // "Master" gain / volume. Controls overall loudness of all sounds
+	alListenerfv(AL_POSITION, listenerPos); // Position, velocity and orientation of listener affect sound...
+	alListenerfv(AL_VELOCITY, listenerVel); // ...reproduction as noted above
+	alListenerfv(AL_ORIENTATION, listenerOri);
+	alListenerf(AL_GAIN, 1.0f);        // "Master" gain / volume. Controls overall loudness of all sounds
 
 	/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	return true;
@@ -142,6 +142,10 @@ void FrontEndSetup()
 
 	//--CAMERA CREATION--//
 	myCamera = myEngine->CreateCamera(kManual, 0.0f, 0.0f, 0.0f);
+
+	//--SOUND--//
+	alSourcePlay(source);
+
 }
 
 void FrontEndUpdate()
@@ -160,7 +164,6 @@ void FrontEndShutdown()
 //--GAME SETUP/SHUTDOWN--//
 void GameSetup()
 {
-
 	//--LOAD MESH/SPRITES--//
 	// - hmmwv.x
 	// - HoverTank01.x
@@ -192,7 +195,7 @@ void GameSetup()
 }
 
 void GameUpdate()
-{
+{	
 	//--FRAMETIME VARIABLE--//
 	float frameTime;
 
